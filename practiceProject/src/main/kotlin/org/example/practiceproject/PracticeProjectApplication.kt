c090673446bc27cfdc4bf14b3d28e23894152195
caporatang <caporatang@naver.com>
2025-08-01 19:18:41 +0900

    Add : 코틀린 web 연습용 모듈 추가

---
+++ practiceProject/src/main/kotlin/org/example/practiceproject/PracticeProjectApplication.kt
@@ -0,0 +1,11 @@
+package org.example.practiceproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeProjectApplication>(*args)
+}
